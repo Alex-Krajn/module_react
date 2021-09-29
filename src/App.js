6c984e3746e6bb983fd4aa148fe75cfdae9fd07f
gortatushka <gortatushka@gmail.com>
2021-09-29 20:16:28 +0300

    add lesson 1 react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import Header from './lesson/Header'
+
+function App() {
+  return (
+    <div>
+      Hello React!
+
+      <Header />
+    </div>
+  );
+}
+
+export default App;
