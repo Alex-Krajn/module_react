fcca0839c0f3e3aa8fd8d0f42b40f53247831b15
gortatushka <gortatushka@gmail.com>
2021-10-04 21:02:33 +0300

    add lesson react 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
 
-import Header from './lesson/Header'
+import Header from './lesson/Header/Header'
+import Functions from './lesson2/Functions'
+import TagsInArray from './lesson2/TagsInArray'
+import States from './lesson2/States'
+import Inputs from './lesson2/Inputs'
+import Textarea from './lesson2/Textarea'
+import Checkbox from './lesson2/Checkbox'
+import Select from './lesson2/Select'
+import Radio from './lesson2/Radio'
 
 function App() {
   return (
@@ -8,6 +16,22 @@ function App() {
       Hello React!
 
       <Header />
+
+      <Functions />
+
+      <TagsInArray />
+
+      <States />
+
+      <Inputs />
+
+      <Textarea />
+
+      <Checkbox />
+
+      <Select />
+
+      <Radio />
     </div>
   );
 }
