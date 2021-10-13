ddcef249e10bc0d306894d317f9c51b51a999e1b
gortatushka <gortatushka@gmail.com>
2021-10-13 20:56:12 +0300

    add request server

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
 import './App.css';
 
-import Header from './lesson/Header/Header'
-import Functions from './lesson2/Functions'
-import TagsInArray from './lesson2/TagsInArray'
-import States from './lesson2/States'
-import Inputs from './lesson2/Inputs'
-import Textarea from './lesson2/Textarea'
-import Checkbox from './lesson2/Checkbox'
-import Select from './lesson2/Select'
-import Radio from './lesson2/Radio'
+import ListRickAndMorty from './components/ListRickAndMorty'
 
 function App() {
   return (
     <div>
-      Hello React!
-
-      <Header />
-
-      <Functions />
-
-      <TagsInArray />
-
-      <States />
-
-      <Inputs />
-
-      <Textarea />
-
-      <Checkbox />
-
-      <Select />
-
-      <Radio />
+      <ListRickAndMorty />
     </div>
   );
 }
